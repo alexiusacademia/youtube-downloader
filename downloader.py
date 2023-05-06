81ea6156766ad0740f267fbf27d2a829e70fd118
Alexius Academia <alexius.academia@gmail.com>
2023-05-07 01:08:03 +0800

    Put the progress to separate thread

--- downloader.py
+++ downloader.py
@@ -7,8 +7,10 @@ if hasattr(ssl, '_create_unverified_context'):
 
 
 class DownloaderDlp:
-    def __init__(self, url: str):
+    def __init__(self, url: str, ui_progress, ui_progress_label):
         self.url = url
+        self.ui_progress = ui_progress
+        self.ui_progress_label = ui_progress_label
 
     def fetch_streams(self):
         """
@@ -59,7 +61,37 @@ class DownloaderDlp:
             format_id (str): The format id selected.
         """
         ydl_options = {
-            'format': format_id
+            'format': format_id,
+            'progress_hooks': [self.update_progress]
         }
         with yt_dlp.YoutubeDL(ydl_options) as ydl:
             ydl.download([self.url])
+
+    def update_progress(self, progress):
+        """
+        Callback for the progress of download.
+
+        Args:
+            progress (dict): Dictionary of the progress, automatically passed.
+        """
+        if progress['status'] == 'downloading':
+            downloaded_bytes = progress['downloaded_bytes']  # Get the current downloaded bytes
+            total_bytes = progress['total_bytes_estimate']  # Get the estimated total bytes
+
+            percent = 0.0
+
+            if total_bytes:
+                percent = round(downloaded_bytes / total_bytes * 100, 2)
+
+                if self.ui_progress:
+                    self.ui_progress.master.after(10, self.update_uis(percent))
+
+    def update_uis(self, percent: float):
+        """
+        Update the label and progress bar of the UI.
+
+        Args:
+            percent (float): The percentage of the download.
+        """
+        self.ui_progress.config(value=percent)
+        self.ui_progress_label.config(text=f'Status: {percent}%')
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from downloader import DownloaderDlp
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+import threading
+
 class MainFrame(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master=master)
@@ -16,20 +18,28 @@ class MainFrame(tk.Frame):
         self.txt_url = ttk.Entry(self.master, width=40)
         btn_fetch_streams = ttk.Button(self.master, 
                                        text='Fetch Streams')
-        btn_fetch_streams.bind('<Button-1>', self.btn_fetch_clicked)
         self.lbl_video_title = ttk.Label(self.master, text='[Title Here...]')
         self.table = ttk.Treeview(self.master, 
                                   show='headings', 
                                   columns=('itag', 'mime_type', 'resolution', 'type'))
 
         tbl = self.table
-        tbl.bind('<Double-1>', self.row_double_clicked)
 
         lbl_url.pack(pady=5)
         self.txt_url.pack(padx=10, pady=5, fill='x')
         btn_fetch_streams.pack(pady=5)
         self.lbl_video_title.pack(pady=5)
         tbl.pack(padx=10, pady=5, fill='both', expand=True)
+
+        self.lbl_percent = ttk.Label(self.master, text='Status: 0%')
+        self.lbl_percent.pack(pady=5)
+
+        self.progress_bar = ttk.Progressbar(self.master, orient='horizontal', mode='determinate', maximum=100, value=0)
+        self.progress_bar.pack(padx=10, pady=5, fill='x')
+
+        # Bindings
+        btn_fetch_streams.bind('<Button-1>', self.btn_fetch_clicked)
+        tbl.bind('<Double-1>', self.row_double_clicked)
 
     def btn_fetch_clicked(self, event):
         url = self.txt_url.get()  # Fetch the URL from the input
@@ -41,7 +51,7 @@ class MainFrame(tk.Frame):
             return
         
         # Initialize the downloader object
-        self.downloader = DownloaderDlp(self.txt_url.get())
+        self.downloader = DownloaderDlp(self.txt_url.get(), self.progress_bar, self.lbl_percent)
 
         # Update the title of the video in the UI
         self.lbl_video_title.config(text=self.downloader.get_title())
@@ -91,8 +101,9 @@ class MainFrame(tk.Frame):
         selected_item = self.table.selection()[0]
         values = self.table.item(selected_item, 'values')
         id = values[0]
-        self.downloader.download(id)
 
+        thread_download = threading.Thread(target=self.downloader.download, args=[id])
+        thread_download.start()
 
 
 if __name__ == '__main__':
